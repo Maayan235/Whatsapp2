3df2b7e4943e50c4d21848ae3ce725aac5370d19
AvitalGol <avitalgol168@gmail.com>
2022-04-15 11:18:51 +0300

    bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { Component } from "react";
 import AllContacts from './Contacts/AllContacts';
 import Chat from './ChatWindow/Chat';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
